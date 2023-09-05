ef9e9c7c3a4c363e6261d72f37cef39d9bc79736
wahidgoni17 <imwahidgoni17@gmail.com>
2023-09-05 20:49:29 +0600

    added post project

--- src/app/(main)/post_project/ProjectForm.jsx
+++ src/app/(main)/post_project/ProjectForm.jsx
@@ -1,9 +1,16 @@
 "use client";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
 const ProjectForm = () => {
   const [skills, setSkills] = useState([]);
+  const [specificCategory, setSpecificCategory] = useState(
+    "Programming & Development"
+  );
+  const [isPublicChecked, setIsPublicChecked] = useState(true);
+  const [isPrivateChecked, setIsPrivateChecked] = useState(false);
   useEffect(() => {
     async function fetchSkills() {
       try {
@@ -15,58 +22,127 @@ const ProjectForm = () => {
     }
     fetchSkills();
   }, []);
+
+  const handleCategoryChange = (event) => {
+    const selectedCategory = event.target.value;
+    setSpecificCategory(selectedCategory);
+  };
+
+  const categoryData = skills.find(
+    (item) => item?.category === specificCategory
+  );
+  const skillsInSpecificCategory = categoryData?.skills;
+
+  const handlePublicChange = () => {
+    setIsPublicChecked(!isPublicChecked);
+    setIsPrivateChecked(false);
+  };
+
+  const handlePrivateChange = () => {
+    setIsPrivateChecked(!isPrivateChecked);
+    setIsPublicChecked(false);
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const SubmitProject = async (data) => {
+    try {
+      const res = await axios.post("http://localhost:5000/post_projects", data);
+      const result = res.data;
+
+      if (result.insertedId) {
+        new Swal({
+          title: "Project Posted Successfully",
+          icon: "success",
+        });
+      } else {
+        console.error("Invalid server response:", result);
+      }
+    } catch (error) {
+      console.error("Error posting project:", error);
+    }
+  };
+
   return (
     <>
-      <div className="border-b pb-5 space-y-2">
-        <h1 className="text-3xl">Post a Project</h1>
+      <div className="border-b text-center py-10 space-y-2">
+        <h1 className="text-3xl font-bold">Post a Project</h1>
+        <h1 className=" border-b-blue-600  w-12 border-2 my-4 mx-auto"></h1>
         <p className="text-xl font-semibold">
           Describe what you need, then receive custom proposals from
           freelancers.
         </p>
       </div>
       <div className="py-10">
-        <h3>
+        <h3 className="text-center">
           Select a relevant category so that freelancers can find your project
         </h3>
       </div>
-      <form>
+      <form
+        onSubmit={handleSubmit(SubmitProject)}
+        className="px-0 md:px-5 space-y-8"
+      >
         <section className="flex flex-col lg:flex-row gap-10">
-          <div className="form-control w-1/2">
+          <div className="form-control w-full md:w-1/2">
             <label className="label">
-              <span className="label-text">Category</span>
+              <span className="label-text uppercase font-semibold">
+                Category
+              </span>
             </label>
-            <select className="select select-bordered">
-              <option disabled selected>
+            <select
+              className="select select-bordered"
+              {...register("category", { required: true })}
+              onChange={handleCategoryChange}
+              value={specificCategory}
+            >
+              <option disabled value="">
                 Select
               </option>
               {skills.map((skill) => (
-                <option key={skill._id}>{skill.category}</option>
+                <option key={skill._id} value={skill.category}>
+                  {skill.category}
+                </option>
               ))}
             </select>
           </div>
-          <div className="form-control w-1/2">
+          <div className="form-control w-full md:w-1/2">
             <label className="label">
-              <span className="label-text">SubCategory</span>
+              <span className="label-text uppercase font-semibold">
+                SubCategory
+              </span>
             </label>
-            <select className="select select-bordered">
+            <select
+              className="select select-bordered"
+              {...register("subCategory", { required: true })}
+            >
               <option disabled selected>
                 Select
               </option>
+              {skillsInSpecificCategory?.map((skill, index) => (
+                <option key={index}>{skill}</option>
+              ))}
             </select>
           </div>
         </section>
         <section>
           <div className="form-control w-full max-w-full">
             <label className="label">
-              <span className="label-text">Project Title</span>
+              <span className="label-text uppercase font-semibold">
+                Project Title
+              </span>
             </label>
             <input
               type="text"
+              {...register("title", { required: true })}
               placeholder="e.g I need a flyer and poster created"
               className="input input-bordered w-full max-w-full"
             />
             <label className="label">
-              <span className="label-text-alt">
+              <span className="label-text text-gray-400">
                 Try to summarise your project in one sentence
               </span>
             </label>
@@ -75,14 +151,17 @@ const ProjectForm = () => {
         <section>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Project Description</span>
+              <span className="label-text uppercase font-semibold">
+                Project Description
+              </span>
             </label>
             <textarea
+              {...register("description", { required: true })}
               className="textarea textarea-bordered h-24"
               placeholder="I need a designer who specialises in professional  UX design"
             ></textarea>
             <label className="label">
-              <span className="label-text-alt">
+              <span className="label-text text-gray-400">
                 Be descriptive, projects with good descriptions are more popular
                 with our freelancers.
               </span>
@@ -90,11 +169,16 @@ const ProjectForm = () => {
           </div>
         </section>
         <section className="flex flex-col lg:flex-row gap-10">
-          <div className="form-control w-full max-w-xs">
+          <div className="form-control w-full md:w-1/2">
             <label className="label">
-              <span className="label-text">Budget Type</span>
+              <span className="label-text uppercase font-semibold">
+                Budget Type
+              </span>
             </label>
-            <select className="select select-bordered">
+            <select
+              className="select select-bordered"
+              {...register("budget_type", { required: true })}
+            >
               <option disabled selected>
                 Select
               </option>
@@ -102,11 +186,16 @@ const ProjectForm = () => {
               <option>Per Hour</option>
             </select>
           </div>
-          <div className="form-control w-full max-w-xs">
+          <div className="form-control w-full md:w-1/2">
             <label className="label">
-              <span className="label-text">Currency</span>
+              <span className="label-text uppercase font-semibold">
+                Currency
+              </span>
             </label>
-            <select className="select select-bordered">
+            <select
+              className="select select-bordered"
+              {...register("currency", { required: true })}
+            >
               <option disabled selected>
                 Select
               </option>
@@ -118,45 +207,65 @@ const ProjectForm = () => {
           </div>
         </section>
         <section className="flex flex-col lg:flex-row gap-10">
-          <div className="form-control w-1/4">
+          <div className="form-control w-1/3">
             <label className="label">
-              <span className="label-text">Budget</span>
+              <span className="label-text uppercase font-semibold">Budget</span>
             </label>
             <input
               type="text"
+              {...register("budget", { required: true })}
               placeholder="10"
               className="input input-bordered w-full max-w-full"
             />
           </div>
         </section>
         <section>
-          <div className="form-control w-[75%]">
+          <div className="form-control w-[50%]">
             <label className="label">
-              <span className="label-text">Freelancer Location</span>
+              <span className="label-text uppercase font-semibold">
+                Freelancer Location
+              </span>
             </label>
-            <select className="select select-bordered">
+            <select
+              className="select select-bordered"
+              {...register("location", { required: true })}
+            >
               <option disabled selected>
-                Select
+                Select Location
               </option>
-              <option>Fixed Price</option>
-              <option>Per Hour</option>
-              <option>Per Hour</option>
+              <option>Remotely (anywhere)</option>
+              <option>Remotely (preferred country)</option>
+              <option>On-Site (specific location)</option>
             </select>
           </div>
         </section>
-        <section>
+        <section className="space-y-2">
           <label className="label">
-            <span className="label-text">Project Visibility</span>
+            <span className="label-text uppercase font-semibold">
+              Project Visibility
+            </span>
           </label>
           <div className="flex gap-4 items-center">
-            <input type="checkbox" className="checkbox" />
+            <input
+              type="checkbox"
+              {...register("visibility_public")}
+              className="checkbox checkbox-accent"
+              checked={isPublicChecked}
+              onChange={handlePublicChange}
+            />
             <p className="hover:underline">
               PUBLIC (All freelancers can view the project post and send
               proposals)
             </p>
           </div>
           <div className="flex gap-4 items-center">
-            <input type="checkbox" className="checkbox" />
+            <input
+              type="checkbox"
+              {...register("visibility_private")}
+              className="checkbox checkbox-accent"
+              checked={isPrivateChecked}
+              onChange={handlePrivateChange}
+            />
             <p className="hover:underline">
               PRIVATE (Only freelancers that you specifically invite can view
               the project post and send proposals)
@@ -166,42 +275,60 @@ const ProjectForm = () => {
         <section>
           <div className="form-control w-[75%]">
             <label className="label">
-              <span className="label-text">
+              <span className="label-text uppercase font-semibold">
                 GET TO KNOW YOUR FREELANCERS (optional)
               </span>
             </label>
-            <select className="select select-bordered">
+            <select
+              className="select select-bordered"
+              {...register("question")}
+            >
               <option disabled selected>
                 Select an Interview Question
               </option>
-              <option>Fixed Price</option>
-              <option>Per Hour</option>
-              <option>Per Hour</option>
+              <option>Why do you like this project?</option>
+              <option>What challenges see in this project?</option>
+              <option>
+                Do you have any suggestions for the implementation or strategy?
+              </option>
+              <option>What are your greatest strengths?</option>
+              <option>How do you usually collaborate with other Buyers?</option>
             </select>
           </div>
         </section>
         <section>
           <div className="form-control w-1/2">
             <label className="label">
-              <span className="label-text">
+              <span className="label-text uppercase font-semibold">
                 ESTIMATED PROJECT DURATION (optional)
               </span>
             </label>
-            <select className="select select-bordered">
+            <select
+              className="select select-bordered"
+              {...register("duration")}
+            >
               <option disabled selected>
                 Select
               </option>
-              <option>Fixed Price</option>
-              <option>Per Hour</option>
-              <option>Per Hour</option>
+              <option>1 day or less</option>
+              <option>less than 1 week</option>
+              <option>1 - 2 weeks</option>
+              <option>3 - 4 weeks</option>
+              <option>1 - 6 months</option>
+              <option>More than 6 months</option>
+              <option>Ongoing</option>
+              <option>Not sure</option>
+              <option>other</option>
             </select>
           </div>
         </section>
-        <input
-          className="btn bg-blue-700 hover:bg-blue-900 text-white rounded-none"
-          type="submit"
-          value="Post Project"
-        />
+        <section className="text-end">
+          <input
+            className="btn bg-blue-700 hover:bg-blue-900 text-white rounded-none"
+            type="submit"
+            value="Post Project"
+          />
+        </section>
       </form>
     </>
   );
