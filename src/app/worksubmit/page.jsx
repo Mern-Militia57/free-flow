46eea258ef30985548f99594d2c7a48a2e2a4694
wahidgoni17 <imwahidgoni17@gmail.com>
2023-09-25 11:50:11 +0600

    merge with development

--- src/app/worksubmit/page.jsx
+++ src/app/worksubmit/page.jsx
@@ -1,31 +1,20 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const UploadFile = () => {
+  const [getFiles, setFiles] = useState([]);
 
-
-    const [getFiles, setFiles] = useState([]);
-
-    useEffect(() => {
-
-      axios.get('http://localhost:5000/getvalues')
-        .then((response) => {
-          setFiles(response.data);
-        })
-        .catch((error) => {
-          console.error('Error fetching files:', error);
-        });
-    }, []);
-
-
-
-
-
-
-
-
-
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/getvalues")
+      .then((response) => {
+        setFiles(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching files:", error);
+      });
+  }, []);
 
   const [file, setFile] = useState(null);
 
@@ -37,36 +26,31 @@ const UploadFile = () => {
     e.preventDefault();
 
     if (!file) {
-      alert('Please select a file to upload.');
+      alert("Please select a file to upload.");
       return;
     }
 
     const formData = new FormData();
-    formData.append('zips', file);
+    formData.append("zips", file);
 
     try {
-      const response = await axios.post('http://localhost:5000/upload', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data', 
-        },
-      });
+      const response = await axios.post(
+        "http://localhost:5000/upload",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
 
       console.log(response.data);
-      alert('File uploaded successfully.');
+      alert("File uploaded successfully.");
     } catch (error) {
-      console.error('Error uploading file:', error.response?.data);
-      alert('Error uploading file: ' + error.response?.data);
+      console.error("Error uploading file:", error.response?.data);
+      alert("Error uploading file: " + error.response?.data);
     }
   };
-
-
-
-
-
-
-
-
-
 
   const [zipData, setZipData] = useState(null);
 
@@ -74,20 +58,19 @@ const UploadFile = () => {
     // Fetch the ZIP data from your server or API
     async function fetchZipData() {
       try {
-        const response = await fetch('http://localhost:5000/getvalues'); // Replace with your server endpoint
+        const response = await fetch("http://localhost:5000/getvalues"); // Replace with your server endpoint
         if (response.ok) {
           const jsonData = await response.json();
           if (Array.isArray(jsonData) && jsonData.length > 0) {
-   
             const zipDataInBase64 = jsonData[0].data;
             const zipData = base64ToArrayBuffer(zipDataInBase64);
             setZipData(zipData);
           }
         } else {
-          console.error('Failed to fetch ZIP data');
+          console.error("Failed to fetch ZIP data");
         }
       } catch (error) {
-        console.error('Error fetching ZIP data:', error);
+        console.error("Error fetching ZIP data:", error);
       }
     }
 
@@ -108,13 +91,14 @@ const UploadFile = () => {
 
   const handleDownloadClick = () => {
     if (zipData) {
-      const blob = new Blob([zipData], { type: 'application/x-zip-compressed' });
+      const blob = new Blob([zipData], {
+        type: "application/x-zip-compressed",
+      });
       const url = window.URL.createObjectURL(blob);
 
-
-      const a = document.createElement('a');
+      const a = document.createElement("a");
       a.href = url;
-      a.download = 'downloaded.zip';
+      a.download = "downloaded.zip";
       document.body.appendChild(a);
       a.click();
 
@@ -124,40 +108,22 @@ const UploadFile = () => {
     }
   };
 
-
-
-
-
-
-
-
-
-
-
-
-
   return (
     <div>
       <h2>Upload a File</h2>
       <form onSubmit={handleSubmit}>
-        <input  type="file" name="zips" onChange={handleFileChange} />
+        <input type="file" name="zips" onChange={handleFileChange} />
         <button type="submit">Upload</button>
       </form>
 
-
-
-      <div className='border border-red-500'>
-        {
-            getFiles?.map(p=><>
-             <li>
-          
-            <button onClick={handleDownloadClick}>Download ZIP</button>
-          </li>
-            
-            
-            </>)
-        }
-
+      <div className="border border-red-500">
+        {getFiles?.map((p) => (
+          <>
+            <li>
+              <button onClick={handleDownloadClick}>Download ZIP</button>
+            </li>
+          </>
+        ))}
       </div>
     </div>
   );
